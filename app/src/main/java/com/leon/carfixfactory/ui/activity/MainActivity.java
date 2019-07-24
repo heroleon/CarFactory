def1d0432d77224f9c92b4631b479b456ec76ff0
dongchaoli <dcl@zhihuigongjiang.com>
2019-07-24 19:00:46 +0800

    1.添加图标、设计首页

--- app/src/main/java/com/leon/carfixfactory/ui/activity/MainActivity.java
+++ app/src/main/java/com/leon/carfixfactory/ui/activity/MainActivity.java
@@ -1,16 +1,11 @@
 package com.leon.carfixfactory.ui.activity;
 
-import android.content.ComponentName;
-import android.content.Intent;
+
 import android.os.Build;
-import android.support.annotation.Nullable;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-
 import com.leon.carfixfactory.R;
-import com.leon.carfixfactory.utils.ZToast;
-
 import java.util.ArrayList;
 
 import butterknife.Bind;
@@ -23,7 +18,6 @@ public class MainActivity extends BaseFragmentActivity {
     ImageView ivScan;
     @Bind(R.id.btn_mine)
     Button mMine;
-    private int mRequestCode = 1000;
 
     @Override
     protected void initView() {
@@ -63,25 +57,5 @@ public class MainActivity extends BaseFragmentActivity {
     public void resetBottomBtn() {
         mHome.setBackgroundResource(R.drawable.homepage);
         mMine.setBackgroundResource(R.drawable.mine);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == mRequestCode) {
-            if (resultCode == 2) {
-                if (data != null) {
-                    ArrayList<String> hphms = data.getStringArrayListExtra("HPHMS");
-                    if (hphms == null) {
-                        ZToast.makeText(this, "车牌识别异常，请重试", 1000).show();
-                        return;
-                    }
-                    if (hphms.size() > 1) {
-                        ZToast.makeText(this, hphms.get(0), 1000).show();
-                    }
-                }
-
-            }
-        }
     }
 }
