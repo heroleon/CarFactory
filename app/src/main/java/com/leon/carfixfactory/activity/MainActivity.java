0bcd38fc0e80c0761fe658f51d2070a197d4e52a
leon <dclforever@163.com>
2019-07-24 19:53:28 +0800

    提交部分数据

--- app/src/main/java/com/leon/carfixfactory/activity/MainActivity.java
+++ app/src/main/java/com/leon/carfixfactory/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.leon.carfixfactory;
+package com.leon.carfixfactory.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.leon.carfixfactory.R;
 
 public class MainActivity extends AppCompatActivity {
 
